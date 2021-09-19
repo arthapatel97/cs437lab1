0fbbf8ae84140049f620e6c5ba3e7f97ea9a4297
stonov <60677967+stonov@users.noreply.github.com>
2021-09-19 02:42:15 -0500

    Added recordObstacle, mapPositionToCell, and other methods to environmengGrid

--- part2/environmentGrid.py
+++ part2/environmentGrid.py
@@ -1,9 +1,21 @@
 import numpy as np
+from numpy.core.defchararray import mod
+
+# Calculates x,y position given angle and distance
+# returns a tuple of (xnew,ynew)
+def getPosition(x_init, y_init, angle, dist):
+    return ( x_init + dist*np.cos(angle) , y_init + dist*np.sin(angle) )
 
 # The environment grid that will contain our robot's surroundings
 # Origin is at the top-left corner
+# The origin of the orientation is parallel to the X-axis, and increasing
+# the angle is counter-clockwise, and decreasing the angle is clockwise
 class EnvironmentGrid:
     def __init__(self, granularity, sideLength):
+        self.initializeEmptyGrid(granularity, sideLength)
+    
+    # Initializes an empty grid with a certain granularity and sideLength
+    def initializeEmptyGrid(self, granularity, sideLength):
         # The size of each square in the environment grid
         # measured in centimeters
         self.granularity = granularity
@@ -23,8 +35,8 @@ class EnvironmentGrid:
         self.robotY = self.sideLength/2
 
         # The start orientation of our little guy.
-        self.orientation = 0
-    
+        self.RobotOrientation = 0
+
     # Calculates the number of cells in a row of the
     # environment grid.
     def getNumCellsPerRow(self):
@@ -34,8 +46,32 @@ class EnvironmentGrid:
     def createEnvironmentGrid(self):
         return np.zeros((self.getNumCellsPerRow(), self.numCellsPerRow()))    
 
+    # Updates the position of the robot
+    def updatePosition(self, timeStep, velocity):
+        self.robotX, self.robotY = getPosition(self.robotX, self.robotY, self.RobotOrientation, velocity * timeStep)
 
-    # Calculates x,y position given angle and distance
-    # returns a tuple of (xnew,ynew)
-    def getPosition(x_init,y_init,angle,dist):
-        return ( x_init + dist*np.cos(angle) , y_init + dist*np.sin(angle) )
+    # Updates the orientation of the robot
+    def updateOrientation(self, timeStep, turnSpeed):
+        self.robotOrientation += timeStep*turnSpeed % 360
+
+    # Checks whether the cell is valid
+    def isValidCell(self, i, j):
+        if (i >= 0 and j >= 0):
+            if (i < np.size(self.map)[0] and j < np.size(self.map)[1]):
+                return True
+        return False
+
+    # Marks an object in the environment as an obstacle
+    # @ angle: The angle of the object relative to our robot's orientation
+    # @ distance: The distance between our robot and the object
+    def recordObstacle(self, angle, distance):
+        objectX, objectY = getPosition(self.robotX, self.robotY, self.RobotOrientation + angle, distance)
+        i, j = self.mapPositionToCell(objectX, objectY)
+        if (self.isValidCell(i, j)):
+            self.map[i,j] = 1
+    
+    # Returns the coordinates of a cell after being given the x and y position
+    def mapPositionToCell(self, x, y):
+        i = int(x/self.granularity)
+        j = int(y/self.granularity)
+    
