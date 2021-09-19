024987ba9d588156e9153627e194f9671387cf2e
stonov <60677967+stonov@users.noreply.github.com>
2021-09-19 00:01:43 -0500

    Initial implementation of environmentGrid

---
+++ part2/environmentGrid.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+# The environment grid that will contain our robot's surroundings
+# Origin is at the top-left corner
+class EnvironmentGrid:
+    def __init__(self, granularity, sideLength):
+        # The size of each square in the environment grid
+        # measured in centimeters
+        self.granularity = granularity
+
+        # The side length of the entire environment grid
+        # measured in centimeters
+        self.sideLength = sideLength
+
+        # The container for the environment grid
+        # A numpy array where 0 denotes there's nothing there,
+        # and 1 denotes there's something there
+        self.map = self.createEnvironmentGrid()
+
+        # The start coordinates of our little guy.
+        # Places him right in the middle
+        self.robotX = self.sideLength/2
+        self.robotY = self.sideLength/2
+
+        # The start orientation of our little guy.
+        self.orientation = 
+    
+    # Calculates the number of cells in a row of the
+    # environment grid.
+    def getNumCellsPerRow(self):
+        return int(self.sideLength/self.granularity)
+    
+    # Creates the container for the environment grid
+    def createEnvironmentGrid(self):
+        return np.zeros((self.getNumCellsPerRow(), self.numCellsPerRow()))    
