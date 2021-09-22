d0989ca16f524aaa7e15cc6e2a111b814459d93f
Sam Hachem <hhache2@illinois.edu>
2021-09-22 00:12:10 -0500

    adding sam's request <3

--- part2/environmentMapping/environment_test.py
+++ part2/environmentMapping/environment_test.py
@@ -1,19 +1,23 @@
 import environmentGrid as env
-import uv_scan as usv
 import numpy as np
 
 GRANULARITY = 5
 SIDE_LENGTH = 100
-IMPACT_DISTANCE = 4000
+IMPACT_DISTANCE = 40
 
 def getMap(granularity=GRANULARITY, sideLength=SIDE_LENGTH, impactDistance=IMPACT_DISTANCE):
     # Sam, put the output of your function here
-    scanOutput = usv.get_enivroment_data()
+    scanOutput = []
 
     testEnvironment = env.EnvironmentGrid(granularity, sideLength, impactDistance)
     testEnvironment.processScanOutput(scanOutput)
 
-    return testEnvironment
+    # added as per Sam's request <3
+    map = testEnvironment.map
+    middle = np.floor(np.shape(map)[0]/2)
+    map[middle, middle] = -2
+
+    return map
 
 if __name__ == "__main__":
     print(getMap())
