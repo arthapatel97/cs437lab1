9837ab45c217eb821ba2aad4d3031688d922cb49
coolguy_69 <edward.passagi@gmail.com>
2021-09-22 00:29:19 -0500

    casted map to int

--- part2/environmentMapping/environment_test.py
+++ part2/environmentMapping/environment_test.py
@@ -1,20 +1,22 @@
 import environmentGrid as env
+import uv_scan as sam
 import numpy as np
 
 GRANULARITY = 5
-SIDE_LENGTH = 100
+SIDE_LENGTH = 90
 IMPACT_DISTANCE = 40
 
 def getMap(granularity=GRANULARITY, sideLength=SIDE_LENGTH, impactDistance=IMPACT_DISTANCE):
     # Sam, put the output of your function here
-    scanOutput = []
+    scanOutput = sam.get_enivroment_data(1)
 
     testEnvironment = env.EnvironmentGrid(granularity, sideLength, impactDistance)
     testEnvironment.processScanOutput(scanOutput)
 
     # added as per Sam's request <3
     map = testEnvironment.map
-    middle = np.floor(np.shape(map)[0]/2)
+    middle = np.int(np.shape(map)[0]/2)
+    print(middle)
     map[middle, middle] = -2
 
     return map
