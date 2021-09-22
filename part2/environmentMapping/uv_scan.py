22a26c23b5dbedc422286b70ff0fc99a374dea0a
Sam Hachem <hhache2@illinois.edu>
2021-09-21 22:55:28 -0500

    Part 2 Step 5 Scanning: added some changes to capturing data

--- part2/environmentMapping/uv_scan.py
+++ part2/environmentMapping/uv_scan.py
@@ -1,10 +1,8 @@
 import picar_4wd as fc 
 
 def get_enivroment_data(angle_interval=2):
-    fc.servo.set_angle(0)
     servo_map = []
-
-    fc.servo.set_angle(fc.max_angle)
+    fc.current_angle = fc.max_angle + angle_interval
     while fc.current_angle >= fc.min_angle:
         fc.current_angle -= angle_interval
         fc.servo.set_angle(fc.current_angle)
@@ -12,4 +10,6 @@ def get_enivroment_data(angle_interval=2):
     return servo_map
 
 if __name__ == "__main__":
-    print(get_enivroment_data())
+    servo_map = get_enivroment_data()
+    print(servo_map)
+    print(len(servo_map))
