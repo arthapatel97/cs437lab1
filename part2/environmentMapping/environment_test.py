7399879c1cd4e3fc796ad05afd5ec6041bc80a30
Sam Hachem <hhache2@illinois.edu>
2021-09-21 23:43:04 -0500

    evnironment testing: added uv scanning fun

--- part2/environmentMapping/environment_test.py
+++ part2/environmentMapping/environment_test.py
@@ -1,13 +1,14 @@
 import environmentGrid as env
+import uv_scan as usv
 import numpy as np
 
 GRANULARITY = 5
 SIDE_LENGTH = 100
-IMPACT_DISTANCE = 40
+IMPACT_DISTANCE = 4000
 
 def getMap(granularity=GRANULARITY, sideLength=SIDE_LENGTH, impactDistance=IMPACT_DISTANCE):
     # Sam, put the output of your function here
-    scanOutput = []
+    scanOutput = usv.get_enivroment_data()
 
     testEnvironment = env.EnvironmentGrid(granularity, sideLength, impactDistance)
     testEnvironment.processScanOutput(scanOutput)
